818dc38f489c8fd4f5c26c1000e17e58793b6e8e
Sarah Parker <sarahlparker4@gmail.com>
2025-04-21 13:18:49 -0400

    moved users/me into routers/auth

--- Final Project/routers/auth.py
+++ Final Project/routers/auth.py
@@ -36,3 +36,38 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         "access_token": access_token,
         "token_type": "bearer"
     }
+
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from fastapi import status
+from fastapi import Request
+
+# These should match what's in utils.auth
+SECRET_KEY = "your_secret_key"
+ALGORITHM = "HS256"
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+@router.get("/users/me")
+async def read_users_me(token: str = Depends(oauth2_scheme)):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Invalid authentication",
+            )
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication",
+        )
+
+    user = get_user(username)
+    if user is None:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="User not found",
+        )
+
+    return {"username": user["username"]}
