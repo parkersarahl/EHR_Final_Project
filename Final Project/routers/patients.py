e7f63b3647c9e97858f03b2416043c7ed0a8b064
Sarah Parker <sarahlparker4@gmail.com>
2025-04-21 12:42:20 -0400

    separated patient routes into routers file

--- Final Project/routers/patients.py
+++ Final Project/routers/patients.py
@@ -9,19 +9,22 @@ router = APIRouter(prefix="/patients", tags=["patients"])
 
 # Pydantic model
 class PatientCreate(BaseModel):
-    name: str
+    last_name: str
+    first_name: str 
     dob: str
 
+# Create a new patient
 @router.post("/")
 def create_patient(patient: PatientCreate, db: Session = Depends(get_db)):
     fhir_data = {
         "resourceType": "Patient",
-        "name": [{"text": patient.name}],
+        "name": [{"text": patient.first_name + " " + patient.last_name}],
         "birthDate": patient.dob
     }
 
     new_patient = Patient(
-        name=patient.name,
+        last_name=patient.last_name,
+        first_name=patient.first_name,
         dob=patient.dob,
         fhir_json=json.dumps(fhir_data)
     )
@@ -35,21 +38,23 @@ def create_patient(patient: PatientCreate, db: Session = Depends(get_db)):
         "fhir_data": fhir_data
     }
 
+# Fetch patient by last name
 @router.get("/search")
 def search_patients(last_name: str, db: Session = Depends(get_db)):
-    matches = db.query(Patient).filter(Patient.name.ilike(f"%{last_name}%")).all()
+    matches = db.query(Patient).filter(Patient.last_name.ilike(f"%{last_name}%")).all()
     if not matches:
         raise HTTPException(status_code=404, detail="No patients found")
 
     return [
         {
             "id": patient.id,
-            "name": patient.name,
+            "name": patient.last_name + " " + patient.first_name,
             "dob": patient.dob
         }
         for patient in matches
     ]
 
+# Fetch a specific patient by ID
 @router.get("/{patient_id}")
 def get_patient(patient_id: int, db: Session = Depends(get_db)):
     patient = db.query(Patient).filter(Patient.id == patient_id).first()
@@ -57,3 +62,16 @@ def get_patient(patient_id: int, db: Session = Depends(get_db)):
         raise HTTPException(status_code=404, detail="Patient not found")
 
     return json.loads(patient.fhir_json)
+
+#Delete a patient by ID
+@router.delete("/{patient_id}")
+def delete_patient(patient_id: int, db: Session = Depends(get_db)):
+    patient = db.query(Patient).filter(Patient.id == patient_id).first()
+    if not patient:
+        raise HTTPException(status_code=404, detail="Patient not found")
+
+    db.delete(patient)
+    db.commit()
+
+    return {"message": "Patient deleted successfully"}
+
